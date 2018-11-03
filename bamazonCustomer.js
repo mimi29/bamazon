26e9c9d45700c982befd629b3b68ce43752eb58c
unknown <ming29398@gmail.com>
2018-11-03 01:48:00 -0700

    added starBuying function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,142 +13,64 @@ var connection = mysql.createConnection({
 
   // Your password
   password: "Summer2018!!",
-  database: "greatBay_DB"
+  database: "bamazon_DB"
 });
 
 // connect to the mysql server and sql database
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   // run the start function after the connection is made to prompt the user
-  start();
+  startBuying();
 });
 
-// function which prompts the user for what action they should take
-function start() {
-  inquirer
-    .prompt({
-      name: "postOrBid",
-      type: "rawlist",
-      message: "Would you like to [POST] an auction or [BID] on an auction?",
-      choices: ["POST", "BID"]
-    })
-    .then(function(answer) {
-      // based on their answer, either call the bid or the post functions
-      if (answer.postOrBid.toUpperCase() === "POST") {
-        postAuction();
-      }
-      else {
-        bidAuction();
-      }
-    });
-}
+function startBuying() {
+  var query = "SELECT * FROM products";
 
-// function to handle posting new items up for auction
-function postAuction() {
-  // prompt for info about the item being put up for auction
-  inquirer
-    .prompt([
+  connection.query(query, function (err, res) {
+    console.log("-------------------------------------------------------------------------");
+    console.log("ID   PRODUCT NAMES                DEPARTMENT NAME  PRICE      TOTAL UNITS")
+    console.log("-------------------------------------------------------------------------");
+
+    for (var i = 0; i < res.length; i++) {
+      console.log(+res[i].item_id + " " + res[i].product_name + "   " + res[i].department_name + "  " + res[i].price + "  " + res[i].stock_quantity);
+    }
+
+    inquirer.prompt([
       {
-        name: "item",
+        name: "itemId",
         type: "input",
-        message: "What is the item you would like to submit?"
+        message: "What is the item id would you like to buy?"
       },
       {
-        name: "category",
+        name: "quantity",
         type: "input",
-        message: "What category would you like to place your auction in?"
-      },
-      {
-        name: "startingBid",
-        type: "input",
-        message: "What would you like your starting bid to be?",
-        validate: function(value) {
-          if (isNaN(value) === false) {
-            return true;
-          }
-          return false;
-        }
-      }
-    ])
-    .then(function(answer) {
-      // when finished prompting, insert a new item into the db with that info
-      connection.query(
-        "INSERT INTO auctions SET ?",
-        {
-          item_name: answer.item,
-          category: answer.category,
-          starting_bid: answer.startingBid,
-          highest_bid: answer.startingBid
-        },
-        function(err) {
-          if (err) throw err;
-          console.log("Your auction was created successfully!");
-          // re-prompt the user for if they want to bid or post
-          start();
-        }
-      );
-    });
-}
-
-function bidAuction() {
-  // query the database for all items being auctioned
-  connection.query("SELECT * FROM auctions", function(err, results) {
-    if (err) throw err;
-    // once you have the items, prompt the user for which they'd like to bid on
-    inquirer
-      .prompt([
-        {
-          name: "choice",
-          type: "rawlist",
-          choices: function() {
-            var choiceArray = [];
-            for (var i = 0; i < results.length; i++) {
-              choiceArray.push(results[i].item_name);
-            }
-            return choiceArray;
-          },
-          message: "What auction would you like to place a bid in?"
-        },
-        {
-          name: "bid",
-          type: "input",
-          message: "How much would you like to bid?"
-        }
-      ])
-      .then(function(answer) {
-        // get the information of the chosen item
-        var chosenItem;
-        for (var i = 0; i < results.length; i++) {
-          if (results[i].item_name === answer.choice) {
-            chosenItem = results[i];
+        message: "How many of this item would you like to buy?",
+      }]).then(function (answer) {
+        var selectedId = parseInt(answer.itemId);
+        var selectedQuantity = answer.quantity;
+        var idx = 0;
+        for (var i = 0; i < res.length; i++) {
+          if (selectedId === res[i].item_id) {
+            idx = i;
+            break;
           }
         }
-
-        // determine if bid was high enough
-        if (chosenItem.highest_bid < parseInt(answer.bid)) {
-          // bid was high enough, so update db, let the user know, and start over
-          connection.query(
-            "UPDATE auctions SET ? WHERE ?",
-            [
-              {
-                highest_bid: answer.bid
-              },
-              {
-                id: chosenItem.id
-              }
-            ],
-            function(error) {
-              if (error) throw err;
-              console.log("Bid placed successfully!");
-              start();
-            }
-          );
+        var selectedProduct = res[idx].product_name;
+        if (selectedQuantity < res[idx].stock_quantity) {
+          console.log("Your total for " + "(" + answer.quantity + ")" + " - " + res[idx].product_name + " is: $" + res[idx].price.toFixed(2) * selectedQuantity);
+          connection.query("UPDATE products SET ? WHERE ?", [{
+            stock_quantity: res[idx].stock_quantity - selectedQuantity
+          }, {
+            item_id: res[idx].item_id
+          }], function (err, res) {
+            startBuying();
+          });
         }
         else {
-          // bid wasn't high enough, so apologize and start over
-          console.log("Your bid was too low. Try again...");
-          start();
+          console.log("Sorry, We do not have enough inventory at this time, we only have " + res[chosenId].StockQuantity + " in our Inventory.");
+          startBuying();
         }
-      });
-  });
+      })
+
+  })
 }
---
+++ product_schema.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS bamazon_DB;
+CREATE database bamazon_DB;
+
+USE bamazon_DB;
+
+CREATE TABLE products (
+  item_id INT NOT NULL,
+  product_name VARCHAR(100) NOT NULL,
+  department_name VARCHAR(50) NULL,
+  price DECIMAL(10,4) NOT NULL DEFAULT,
+  stock_quantity INT NOT NULL DEFAULT 0,
+  PRIMARY KEY (item_id)
+);
+
+
